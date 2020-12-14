95622e927785a7ffbb704b6d94dfea3e63135fa0
Elvis Landi <elandi13@yahoo.com>
2020-12-14 09:26:24 -0500

    done

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,59 @@
+require 'pry'
 class Instructor
+
+    attr_accessor :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def boatingtest
+        Boatingtest.all.select{|bt|bt.instructor == self}
+    end
+
+    def all_students
+        self.boatingtest.map(&:student)
+    end
+
+    def all_passed_tests
+        binding.pry
+        self.boatingtest.select{|bt|bt.status == "passed"}
+    end
+
+    def passed_tests
+        self.all_passed_tests.map{|test|test.status == "passed"}
+    end
+
+    def failed_tests
+        self.all_passed_tests.map{|test|test.status == "failed"}
+    end
+
+    def find_student(name)
+        Student.all.find{|student| student.name == name}
+    end
+
+    def find_test(name)
+        BoatingTest.all.find{|test| test.name == name}
+    end
+
+    def pass_student(student, name)
+        test = find_test(name)
+        if test && test.student.name == name
+            test.status = "passed"
+        else
+            student = find_student(name)
+        BoatingTest.new(student, name, "passed", self)
+        end
+    end
+
+
+        
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,40 @@
 class Student
 
+    attr_accessor :student_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @name = student_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def boatingtest
+        Boatingtest.all.select{|bt|bt.student == self}
+    end
+
+    def all_instructor
+        self.boatingtest.map(&:instructor)
+    end
+
+    def add_boating_test(name, status, instructor)
+        self.boatingtest(self, name, status, instructor)
+    end
+
+    def self.find_student(student_first_name)
+        self.all.find{|student| student.first_name == student_first_name}
+    end
+
+    def grade_percentage 
+        studentTests = BoatingTest.all.find_all{|test| test.student.first_name == self.first_name}
+        totalTests = studentTests.length.to_f
+        totalPassed = studentTests.find_all{|test| test.status == "passed"}
+        numPassed = totalPassed.length.to_f
+        percent = (numPassed/totalTests)*100
+    end
+
 end
